d9f7c0a9269104ed2e617e5706b064478931b911
KrisHenriJ <kris.henri.jarvi@tktk.ee>
2023-12-15 23:44:38 +0200

    week15

---
+++ Week15OOPAndFiles/Program.cs
@@ -0,0 +1,18 @@
+﻿
+string[] data = GetDataFromMyFile();
+ReadDataFromArray(data);
+
+
+static string[] GetDataFromMyFile()
+{
+    string filePath = @"/Users/kris/projects/movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach(string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
